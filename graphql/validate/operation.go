bca33d6de7b5725a9ced1e2b7680480267383760
zengdiv <div@light-speak.com>
2024-10-14 20:54:18 +0800

    feat: vaild query args

--- graphql/validate/operation.go
+++ graphql/validate/operation.go
@@ -1,16 +1,27 @@
 package validate
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/light-speak/lighthouse/errors"
 	"github.com/light-speak/lighthouse/graphql/ast"
+	"github.com/light-speak/lighthouse/log"
 	"github.com/light-speak/lighthouse/utils"
 )
+
+var operationArgs = map[string]*ast.ArgumentNode{}
 
 func validateOperation(node ast.Node) error {
 
 	operNode := node.(*ast.OperationNode)
 
-	typeMap, ok := p.TypeMap[utils.UcFirst(string(operNode.Type))]
+	err := validateOperationArgs(operNode.Args)
+	if err != nil {
+		return err
+	}
+
+	_, ok := p.TypeMap[utils.UcFirst(string(operNode.Type))]
 	if !ok {
 		return &errors.ValidateError{
 			Node:    operNode,
@@ -18,61 +29,156 @@ func validateOperation(node ast.Node) error {
 		}
 	}
 
-	// 步骤1：获取操作的字段
 	fields := node.GetFields()
 
-	// 步骤2：验证字段是否存在 验证字段返回类型是否有效，并验证子字段是否匹配返回类型
 	for _, field := range fields {
-		if fieldNode, err := validateOperationFieldType(field, typeMap); fieldNode != nil {
-			if err := validateFieldReturnType(field, fieldNode); err != nil {
+		err = validateFieldArgs(field)
+		if err != nil {
+			return err
+		}
+	}
+
+	fields, _ = validateOperationFields(fields)
+
+	var printChildren func(children []*ast.FieldNode, depth int)
+	printChildren = func(children []*ast.FieldNode, depth int) {
+		for _, f := range children {
+			indent := strings.Repeat("  ", depth)
+			log.Debug().Msgf("%s子字段名称: %+v", indent, f.GetName())
+			if len(f.Children) > 0 {
+				printChildren(f.Children, depth+1)
+			}
+		}
+	}
+
+	for _, field := range fields {
+		printChildren(field.Children, 0)
+	}
+
+	return nil
+}
+
+func validateOperationArgs(args []*ast.ArgumentNode) error {
+	for _, arg := range args {
+		if arg.Type == nil && arg.Value.Type == nil {
+			return &errors.ValidateError{
+				Node:    arg,
+				Message: fmt.Sprintf("argument %s type is invaild", arg.GetName()),
+			}
+		}
+		operationArgs[arg.GetName()] = arg
+	}
+	return nil
+}
+
+func validateFieldArgs(field *ast.FieldNode) error {
+	// 找到定义的接口
+	t := string(field.Parent.(*ast.OperationNode).Type)
+	defField := &ast.FieldNode{}
+	for _, f := range p.TypeMap[utils.UcFirst(t)].Fields {
+		if f.Name == field.GetName() {
+			defField = f
+			break
+		}
+	}
+
+	// 获取后端定义的接口参数和类型
+	defArgMap := make(map[string]*ast.ArgumentNode)
+	for _, arg := range defField.Args {
+		defArgMap[arg.Name] = arg
+	}
+
+	// 获取前端输入的参数和类型
+	argMap := make(map[string]*ast.ArgumentNode)
+
+	for _, arg := range field.Args {
+		argMap[arg.GetName()] = arg
+	}
+
+	validateListArg := func(defArg *ast.ArgumentNode, arg *ast.ArgumentNode) error {
+		// log.Debug().Msgf("defArg: %+v", defArg.Type)
+		log.Debug().Msgf("arg: %+v", arg.Type.TypeCategory)
+		if defArg.Type.IsList != arg.Type.IsList {
+			return &errors.ValidateError{
+				Node:    field,
+				Message: fmt.Sprintf("argument %s type is invaild", arg.Name),
+			}
+		}
+		if !defArg.Type.IsList {
+			vaild := defArg.Type.Name == arg.Type.Name && defArg.Type.IsList == arg.Type.IsList && defArg.Type.IsNonNull == arg.Type.IsNonNull
+			if !vaild {
+				return &errors.ValidateError{
+					Node:    field,
+					Message: fmt.Sprintf("argument %s type is invaild", arg.Name),
+				}
+			}
+		}
+		return nil
+	}
+
+	for name, defArg := range defArgMap {
+		// log.Debug().Msgf("defArg name: %+v", name)
+		// log.Debug().Msgf("defArg: %+v", defArg.Value.Children[0])
+		log.Debug().Msgf("arg: %+v", argMap[name].Type)
+		// 必传参数
+		if defArg.Type.IsNonNull {
+			if _, ok := argMap[name]; !ok {
+				return &errors.ValidateError{
+					Node:    field,
+					Message: fmt.Sprintf("argument %s is required", name),
+				}
+			}
+		}
+
+		// list 参数，递归检查内层元素是否一致
+		if defArg.Type.IsList {
+			err := validateListArg(defArg, argMap[name])
+			if err != nil {
 				return err
 			}
+		}
+	}
+
+	return nil
+}
+
+func validateOperationFields(fields []*ast.FieldNode) ([]*ast.FieldNode, error) {
+	newFields := make(map[string]*ast.FieldNode)
+	var res []*ast.FieldNode
+
+	var err error
+	processField := func(f *ast.FieldNode) {
+		if _, ok := newFields[f.GetName()]; !ok {
+			res = append(res, f)
+			newFields[f.GetName()] = f
+		}
+	}
+
+	for _, field := range fields {
+		if field.Type != nil && field.Type.TypeCategory == ast.NodeTypeFragment {
+			fragment := getValueTypeFragment(field.GetName())
+			if fragment == nil {
+				return nil, &errors.ValidateError{
+					Node:    field,
+					Message: fmt.Sprintf("fragment %s not found", field.GetName()),
+				}
+			}
+			for _, f := range fragment.Fields {
+				processField(f)
+			}
 		} else {
-			return err
+			processField(field)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 
-	// 步骤4：递归验证子字段
-	// for _, field := range fields {
-	// 	if err := validateChildFields(field); err != nil {
-	// 		return err
-	// 	}
-	// }
-
-	return nil
-}
-
-func validateOperationFieldType(field *ast.FieldNode, typeMap *ast.TypeNode) (*ast.FieldNode, error) {
-
-	// 获取 typeMap 中的字段
-	typeMapFields := typeMap.GetFields()
-
-	// 检查 field 是否存在于 typeMap 中
-	for _, typeMapField := range typeMapFields {
-		if typeMapField.GetName() == field.GetName() {
-			return typeMapField, nil
+	for _, field := range res {
+		if len(field.Children) > 0 {
+			field.Children, _ = validateOperationFields(field.Children)
 		}
 	}
 
-	return nil, &errors.ValidateError{
-		Node:    field,
-		Message: "字段在操作类型中不存在",
-	}
-
-}
-
-func validateFieldReturnType(field *ast.FieldNode, typeField *ast.FieldNode) error {
-	// log.Debug().Msgf("field: %+v", field)
-	// log.Debug().Msgf("typeField: %+v", typeField.Type)
-	// log.Debug().Msgf("32131231232131231231231231312312312 %+v", getValueTypeType(typeField.Type.Name))
-	return nil
-}
-
-func validateChildFields(field *ast.FieldNode) error {
-	for _, childField := range field.Children {
-		if err := validateOperation(childField); err != nil {
-			return err
-		}
-	}
-	return nil
+	return res, nil
 }
